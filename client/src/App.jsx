6ffb202f8fb738fe7524d6c30069478ffa879993
ROHIT <rk7706254@gamil.com>
2024-02-17 16:30:09 +0530

    complete fild section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
-import { NavbarWithMegaMenu } from './components/Navbar';
+import Chat from "./pages/Chat/Chat";
+import Home from "./pages/Home/Home";
 const App = () => {
   
 
   return (
    
-    <div className=" min-h-screen   bg-gray-300 flex flex-col font-inter">
+    <div className="w-screen  min-h-screen  bg-blue-gray-500 flex flex-col font-inter">
       
+        {/* <NavbarWithMegaMenu /> */}
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/chat" element={<Chat />} />
+          {/* <Route path="/blog" element={<Blog />}/> */}
 
-
-
-      
-          
+          {/* <Route path="*" element={<Error />} /> */}
+        </Routes>
+        <Footer />
       </div>
    
   );
